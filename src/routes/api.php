d434444bc590008e1fe296b4f238b9bf4e6826a3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-03-23 11:00:34 +0100

    api

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,8 +5,8 @@
 | LANGS
 |----------------------------------
 */
-Route::get('api/langs',                                         ['as' => 'lang',                            'uses' => 'Syscover\Admin\Controllers\LangController@index']);
-Route::get('api/langs/{id}',                                    ['as' => 'showLang',                        'uses' => 'Syscover\Admin\Controllers\LangController@show']);
+Route::get('api/v1/langs',                                         ['as' => 'lang',                            'uses' => 'Syscover\Admin\Controllers\LangController@index']);
+Route::get('api/v1/langs/{id}',                                    ['as' => 'showLang',                        'uses' => 'Syscover\Admin\Controllers\LangController@show']);
 
 
 /*
@@ -14,77 +14,77 @@ Route::get('api/langs/{id}',                                    ['as' => 'showLa
 | COUNTRIES
 |----------------------------------
 */
-Route::get('api/countries/{lang?}',                             ['as' => 'country',                         'uses' => 'Syscover\Admin\Controllers\CountryController@index']);
-Route::get('api/countries/{id}/{lang}',                         ['as' => 'showCountry',                     'uses' => 'Syscover\Admin\Controllers\CountryController@show']);
+Route::get('api/v1/countries/{lang?}',                             ['as' => 'country',                         'uses' => 'Syscover\Admin\Controllers\CountryController@index']);
+Route::get('api/v1/countries/{id}/{lang}',                         ['as' => 'showCountry',                     'uses' => 'Syscover\Admin\Controllers\CountryController@show']);
 
 /*
 |----------------------------------
 | TERRITORIAL AREAS 1
 |----------------------------------
 */
-Route::get('api/territorialareas1/{lang?}',                     ['as' => 'territorialArea1',                'uses' => 'Syscover\Admin\Controllers\TerritorialArea1Controller@index']);
-Route::get('api/territorialareas1/{id}/{lang}',                 ['as' => 'showTerritorialArea1',            'uses' => 'Syscover\Admin\Controllers\TerritorialArea1Controller@show']);
+Route::get('api/v1/territorialareas1/{lang?}',                     ['as' => 'territorialArea1',                'uses' => 'Syscover\Admin\Controllers\TerritorialArea1Controller@index']);
+Route::get('api/v1/territorialareas1/{id}/{lang}',                 ['as' => 'showTerritorialArea1',            'uses' => 'Syscover\Admin\Controllers\TerritorialArea1Controller@show']);
 
 /*
 |----------------------------------
 | TERRITORIAL AREAS 2
 |----------------------------------
 */
-Route::get('api/territorialareas2',                             ['as' => 'territorialArea2',                'uses' => 'Syscover\Admin\Controllers\TerritorialArea2Controller@index']);
-Route::get('api/territorialareas2/{id}',                        ['as' => 'showTerritorialArea2',            'uses' => 'Syscover\Admin\Controllers\TerritorialArea2Controller@show']);
+Route::get('api/v1/territorialareas2',                             ['as' => 'territorialArea2',                'uses' => 'Syscover\Admin\Controllers\TerritorialArea2Controller@index']);
+Route::get('api/v1/territorialareas2/{id}',                        ['as' => 'showTerritorialArea2',            'uses' => 'Syscover\Admin\Controllers\TerritorialArea2Controller@show']);
 
 /*
 |----------------------------------
 | TERRITORIAL AREAS 3
 |----------------------------------
 */
-Route::get('api/territorialareas3',                             ['as' => 'territorialArea3',                'uses' => 'Syscover\Admin\Controllers\TerritorialArea3Controller@index']);
-Route::get('api/territorialareas3/{id}',                        ['as' => 'showTerritorialArea3',            'uses' => 'Syscover\Admin\Controllers\TerritorialArea3Controller@show']);
+Route::get('api/v1/territorialareas3',                             ['as' => 'territorialArea3',                'uses' => 'Syscover\Admin\Controllers\TerritorialArea3Controller@index']);
+Route::get('api/v1/territorialareas3/{id}',                        ['as' => 'showTerritorialArea3',            'uses' => 'Syscover\Admin\Controllers\TerritorialArea3Controller@show']);
 
 /*
 |----------------------------------
 | PROFILES
 |----------------------------------
 */
-Route::get('api/profiles',                                      ['as' => 'profile',                         'uses' => 'Syscover\Admin\Controllers\ProfileController@index']);
-Route::get('api/profiles/{id}',                                 ['as' => 'showProfile',                     'uses' => 'Syscover\Admin\Controllers\ProfileController@show']);
+Route::get('api/v1/profiles',                                      ['as' => 'profile',                         'uses' => 'Syscover\Admin\Controllers\ProfileController@index']);
+Route::get('api/v1/profiles/{id}',                                 ['as' => 'showProfile',                     'uses' => 'Syscover\Admin\Controllers\ProfileController@show']);
 
 /*
 |----------------------------------
 | ACTIONS
 |----------------------------------
 */
-Route::get('api/actions',                                       ['as' => 'action',                          'uses' => 'Syscover\Admin\Controllers\ActionController@index']);
-Route::get('api/actions/{id}',                                  ['as' => 'showAction',                      'uses' => 'Syscover\Admin\Controllers\ActionController@show']);
+Route::get('api/v1/actions',                                       ['as' => 'action',                          'uses' => 'Syscover\Admin\Controllers\ActionController@index']);
+Route::get('api/v1/actions/{id}',                                  ['as' => 'showAction',                      'uses' => 'Syscover\Admin\Controllers\ActionController@show']);
 
 /*
 |----------------------------------
 | USERS
 |----------------------------------
 */
-Route::get('api/users',                                         ['as' => 'user',                            'uses' => 'Syscover\Admin\Controllers\UserController@index']);
-Route::get('api/users/{id}',                                    ['as' => 'showUser',                        'uses' => 'Syscover\Admin\Controllers\UserController@show']);
+Route::get('api/v1/users',                                         ['as' => 'user',                            'uses' => 'Syscover\Admin\Controllers\UserController@index']);
+Route::get('api/v1/users/{id}',                                    ['as' => 'showUser',                        'uses' => 'Syscover\Admin\Controllers\UserController@show']);
 
 /*
 |----------------------------------
 | PACKAGES
 |----------------------------------
 */
-Route::get('api/packages',                                      ['as' => 'package',                         'uses' => 'Syscover\Admin\Controllers\PackageController@index']);
-Route::get('api/packages/{id}',                                 ['as' => 'showPackage',                     'uses' => 'Syscover\Admin\Controllers\PackageController@show']);
+Route::get('api/v1/packages',                                      ['as' => 'package',                         'uses' => 'Syscover\Admin\Controllers\PackageController@index']);
+Route::get('api/v1/packages/{id}',                                 ['as' => 'showPackage',                     'uses' => 'Syscover\Admin\Controllers\PackageController@show']);
 
 /*
 |----------------------------------
 | RESOURCES
 |----------------------------------
 */
-Route::get('api/resources',                                     ['as' => 'resource',                        'uses' => 'Syscover\Admin\Controllers\ResourceController@index']);
-Route::get('api/resources/{id}',                                ['as' => 'showResource',                    'uses' => 'Syscover\Admin\Controllers\ResourceController@show']);
+Route::get('api/v1/resources',                                     ['as' => 'resource',                        'uses' => 'Syscover\Admin\Controllers\ResourceController@index']);
+Route::get('api/v1/resources/{id}',                                ['as' => 'showResource',                    'uses' => 'Syscover\Admin\Controllers\ResourceController@show']);
 
 /*
 |----------------------------------
 | CRON JOBS
 |----------------------------------
 */
-Route::get('api/cron-jobs',                                     ['as' => 'cronJob',                        'uses' => 'Syscover\Admin\Controllers\CronJobController@index']);
-Route::get('api/cron-jobs/{id}',                                ['as' => 'showCronJob',                    'uses' => 'Syscover\Admin\Controllers\CronJobController@show']);
+Route::get('api/v1/cron-jobs',                                     ['as' => 'cronJob',                        'uses' => 'Syscover\Admin\Controllers\CronJobController@index']);
+Route::get('api/v1/cron-jobs/{id}',                                ['as' => 'showCronJob',                    'uses' => 'Syscover\Admin\Controllers\CronJobController@show']);
