0300de3a140a82e7ee59450e1fb312bc4133db8b
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-12 19:52:58 +0200

    minor fix

--- src/routes/api.php
+++ src/routes/api.php
@@ -19,6 +19,7 @@ Route::delete('api/v1/admin/langs/{id}',                                 ['as' =
 */
 Route::get('api/v1/admin/countries/{lang?}',                             ['as' => 'country',                         'uses' => 'Syscover\Admin\Controllers\CountryController@index']);
 Route::get('api/v1/admin/countries/{id}/{lang}',                         ['as' => 'showCountry',                     'uses' => 'Syscover\Admin\Controllers\CountryController@show']);
+Route::post('api/v1/admin/countries/search',                             ['as' => 'searchCountry',                   'uses' => 'Syscover\Admin\Controllers\CountryController@search']);
 Route::post('api/v1/admin/countries',                                    ['as' => 'storeCountry',                    'uses' => 'Syscover\Admin\Controllers\CountryController@store']);
 Route::put('api/v1/admin/countries/{id}/{lang}',                         ['as' => 'updateAction',                    'uses' => 'Syscover\Admin\Controllers\CountryController@update']);
 Route::delete('api/v1/admin/countries/{id}/{lang?}',                     ['as' => 'destroyAction',                   'uses' => 'Syscover\Admin\Controllers\CountryController@destroy']);
