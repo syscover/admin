1938e49754929c2037dc85c38444c0f0d9c9c37b
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-05-08 21:01:57 +0200

    custom field

--- src/Syscover/Admin/Controllers/FieldController.php
+++ src/Syscover/Admin/Controllers/FieldController.php
@@ -18,10 +18,51 @@ class FieldController extends CoreController
     {
         try
         {
-            $object = Field::create([
-                'name'          => $request->input('name'),
-                'resource_id'   => $request->input('resource_id')
-            ]);
+            // if there is id, is a new language object
+            if($request->has('id'))
+            {
+                // get object to update data and data_lang field
+                $field      = Field::find($request->input('id'));
+
+                // get values
+                $dataLang   = json_decode($field->data_lang, true);
+                $labels     = json_decode($field->labels, true);
+
+                // set values
+                $dataLang['langs'][]                    = $request->input('lang');      // set data_lang
+                $labels[$request->input('lang')]   = $request->input('label');     // set labels
+
+                Field::where('id' ,$request->input('id'))->update([
+                    'data_lang' => json_encode($dataLang),
+                    'labels'    => json_encode($labels)
+                ]);
+
+                $object = Field::find($request->input('id'));
+            }
+            else
+            {
+                $id = Field::max('id');
+                $id++;
+
+                // create new object
+                $object = Field::create([
+                    'id'                => $id,
+                    'field_group_id'    => $request->input('field_group_id'),
+                    'name'              => $request->input('name'),
+                    'labels'            => json_encode([$request->input('lang') => $request->input('label')]),
+                    'field_type_id'     => $request->input('field_type_id'),
+                    'field_type_name'   => $request->input('field_type_name', ''),
+                    'data_type_id'      => $request->input('data_type_id'),
+                    'data_type_name'    => $request->input('data_type_name', ''),
+                    'required'          => $request->input('required'),
+                    'sort'              => $request->input('sort'),
+                    'max_length'        => $request->input('max_length'),
+                    'pattern'           => $request->input('pattern'),
+                    'label_size'        => $request->input('label_size'),
+                    'field_size'        => $request->input('field_size'),
+                    'data_lang'         => Field::addLangDataRecord($request->input('lang'))
+                ]);
+            }
         }
         catch (\Exception $e)
         {
