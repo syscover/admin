1aee5c6c00ef1f85bf6984c0797f1f6e93964a4d
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-05-15 13:18:38 +0200

    delete test

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-
 Route::get('api/v1/admin/test/create',                                   ['as' => 'adminTestCreate',                         'uses' => 'Syscover\Admin\Controllers\TestController@testCreate']);
 Route::get('api/v1/admin/test/update',                                   ['as' => 'adminTestUpdate',                         'uses' => 'Syscover\Admin\Controllers\TestController@testUpdate']);
-Route::get('api/v1/admin/test',                                   ['as' => 'adminTest',                         'uses' => 'Syscover\Admin\Controllers\TestController@test']);
 
 /*
 |----------------------------------
