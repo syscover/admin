57ed55acfb4654f7005501033832f2433a2dc886
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-08-29 15:52:31 +0200

    Add trait geolocalizable

---
+++ src/Syscover/Admin/Traits/Geolocalizable.php
@@ -0,0 +1,23 @@
+<?php namespace Syscover\Admin\Traits;
+
+use Syscover\Admin\Models\Country;
+
+trait Geolocalizable
+{
+    public function country()
+    {
+        return $this->belongsTo(Country::class, 'country_id');
+    }
+
+    public function getTerritorialArea($zone)
+    {
+        switch ($zone) {
+            case 'territorial_areas_1':
+                return $this->{'territorial_area_1_id'};
+            case 'territorial_areas_2':
+                return $this->{'territorial_area_2_id'};
+            case 'territorial_areas_3':
+                return $this->{'territorial_area_3_id'};
+        }
+    }
+}
