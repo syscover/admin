32d314981acfb66df0c19f99e414462e460d32c7
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-30 20:01:00 +0200

    improve services

--- src/Syscover/Admin/Services/LangService.php
+++ src/Syscover/Admin/Services/LangService.php
@@ -6,13 +6,13 @@ class LangService
 {
     public static function create($object)
     {
-        LangService::check($object);
+        LangService::checkCreate($object);
         return Lang::create(LangService::builder($object));
     }
 
     public static function update($object, $ix)
     {
-        LangService::check($object);
+        LangService::checkUpdate($object);
         Lang::where('ix', $ix)->update(LangService::builder($object));
 
         return Lang::find($ix);
@@ -21,21 +21,18 @@ class LangService
     private static function builder($object)
     {
         $object = collect($object);
-        $data = [];
-
-        if($object->has('id'))      $data['id'] = $object->get('id');
-        if($object->has('name'))    $data['name'] = $object->get('name');
-        if($object->has('icon'))    $data['icon'] = $object->get('icon');
-        if($object->has('sort'))    $data['sort'] = $object->get('sort');
-        if($object->has('active'))  $data['active'] = $object->get('active');
-
-        return $data;
+        return $object->only('id', 'name', 'icon', 'sort', 'active')->toArray();
     }
 
-    private static function check($object)
+    private static function checkCreate($object)
     {
         if(empty($object['id']))     throw new \Exception('You have to define a id field to create a lang');
         if(empty($object['name']))   throw new \Exception('You have to define a name field to create a lang');
         if(empty($object['sort']))   throw new \Exception('You have to define a sort field to create a lang');
+    }
+
+    private static function checkUpdate($object)
+    {
+        if(empty($object['ix']))     throw new \Exception('You have to define a ix field to update a lang');
     }
 }
