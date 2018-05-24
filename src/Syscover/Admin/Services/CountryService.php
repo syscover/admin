91103c67260f78aa082f694db16bf90d0512860c
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-05-24 19:22:10 +0200

    set country services

--- src/Syscover/Admin/Services/CountryService.php
+++ src/Syscover/Admin/Services/CountryService.php
@@ -2,48 +2,51 @@
 
 use Syscover\Admin\Models\Country;
 
-/**
- * Class CountryService
- * @package Syscover\Admin\Service
- */
 class CountryService
 {
-    /**
-     * @param  array    $object     contain properties of country
-     * @return \Syscover\Admin\Models\Country
-     */
     public static function create($object)
     {
+        CountryService::checkCreate($object);
+
         $object['data_lang'] = Country::addDataLang($object['lang_id'], $object['id']);
 
-        return Country::create($object);
+        return Country::create(CountryService::builder($object));
     }
 
-    /**
-     * @param array     $object     contain properties of country
-     * @return \Syscover\Admin\Models\Country
-     */
     public static function update($object)
     {
+        CountryService::checkUpdate($object);
+
+        if(empty($object['zones']) && is_array($object['zones'])) $object['zones'] = null;
+        if(! empty($object['zones']) && (is_array($object['zones']) && count($object['zones']) > 0)) $object['zones'] = json_encode($object['zones']);
+
+        Country::where('id', $object['id'])->update(CountryService::builder($object, ['id', 'sort', 'prefix', 'zones']));
+        Country::where('ix', $object['ix'])->update(CountryService::builder($object, ['name', 'territorial_area_1', 'territorial_area_2', 'territorial_area_3']));
+
+        return Country::find($object['ix']);
+    }
+
+    private static function builder($object, $filterKeys = null)
+    {
         $object = collect($object);
+        if($filterKeys) return $object->only($filterKeys)->toArray();
 
-        Country::where('ix', $object->get('ix'))
-            ->update([
-                'name'                  => $object->get('name'),
-                'sort'                  => $object->get('sort'),
-                'territorial_area_1'    => $object->get('territorial_area_1'),
-                'territorial_area_2'    => $object->get('territorial_area_2'),
-                'territorial_area_3'    => $object->get('territorial_area_3')
-            ]);
+        return $object->only(['id', 'lang_id', 'name', 'slug', 'sort', 'prefix', 'territorial_area_1', 'territorial_area_2', 'territorial_area_3', 'zones', 'data_lang'])->toArray();
+    }
 
-        // common data
-        Country::where('ix', $object->get('ix'))
-            ->update([
-                'id'        => $object->get('id'),
-                'prefix'    => $object->get('prefix'),
-                'zones'     => $object->get('zones') === null || (is_array($object->get('zones')) && count($object->get('zones')) == 0) ? null : json_encode($object->get('zones'))
-            ]);
+    private static function checkCreate($object)
+    {
+        if(empty($object['id']))        throw new \Exception('You have to define a id field to create a country');
+        if(empty($object['lang_id']))   throw new \Exception('You have to define a lang_id field to create a country');
+        if(empty($object['name']))      throw new \Exception('You have to define a name field to create a country');
+        if(empty($object['slug']))      throw new \Exception('You have to define a slug field to create a country');
+        if(! empty($object['zones']) && ! is_array($object['zones'])) throw new \Exception('Zones field has to be a array to create a country');
+    }
 
-        return Country::find($object->get('ix'));
+    private static function checkUpdate($object)
+    {
+        if(empty($object['ix'])) throw new \Exception('You have to define a ix field to update a country');
+        if(empty($object['id'])) throw new \Exception('You have to define a id field to update a country');
+        if(! empty($object['zones']) && ! is_array($object['zones'])) throw new \Exception('Zones field has to be a array to update a country');
     }
 }
