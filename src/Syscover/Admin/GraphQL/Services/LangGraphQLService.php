6ac129c3e1c82b55b6a2d853eaa6084afb2aa181
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-21 16:47:19 +0200

    lang graphql

---
+++ src/Syscover/Admin/GraphQL/Services/LangGraphQLService.php
@@ -0,0 +1,11 @@
+<?php namespace Syscover\Admin\GraphQL\Services;
+
+use Syscover\Admin\Models\Lang;
+use Syscover\Admin\Services\LangService;
+use Syscover\Core\GraphQL\Services\CoreGraphQLService;
+
+class LangGraphQLService extends CoreGraphQLService
+{
+    protected $model = Lang::class;
+    protected $service = LangService::class;
+}
