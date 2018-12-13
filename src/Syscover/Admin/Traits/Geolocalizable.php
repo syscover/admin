271ef8beb6efea3d91d1344f209a3ace9ed0089a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-12-13 23:08:41 +0100

    add countries relation to Geolocalizable Trait

--- src/Syscover/Admin/Traits/Geolocalizable.php
+++ src/Syscover/Admin/Traits/Geolocalizable.php
@@ -9,9 +9,15 @@ trait Geolocalizable
         return $this->belongsTo(Country::class, 'country_id', 'id');
     }
 
+    public function countries()
+    {
+        return $this->hasMany(Country::class, 'country_id', 'id');
+    }
+
     public function getTerritorialArea($zone)
     {
-        switch ($zone) {
+        switch ($zone)
+        {
             case 'territorial_areas_1':
                 return $this->{'territorial_area_1_id'};
             case 'territorial_areas_2':
