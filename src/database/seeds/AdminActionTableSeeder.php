9ccce60e9a4d0bed9af6bb94d6a76871dd7dbf4b
Carlos Rodríguez Palacín <info@digitalh2.com>
2019-02-01 00:09:59 +0100

    manage resources

--- src/database/seeds/AdminActionTableSeeder.php
+++ src/database/seeds/AdminActionTableSeeder.php
@@ -8,12 +8,13 @@ class AdminActionTableSeeder extends Seeder
     public function run()
     {
         Action::insert([
-            ['id' => 'access',  'name' => 'Access'],
-            ['id' => 'create',  'name' => 'Create'],
-            ['id' => 'delete',  'name' => 'Delete'],
-            ['id' => 'edit',    'name' => 'Edit'],
-            ['id' => 'list',    'name' => 'List'],
-            ['id' => 'show',    'name' => 'Show']
+            ['id' => 'access',      'name' => 'Access'],
+            ['id' => 'create',      'name' => 'Create'],
+            ['id' => 'create-lang', 'name' => 'Create language'],
+            ['id' => 'delete',      'name' => 'Delete'],
+            ['id' => 'edit',        'name' => 'Edit'],
+            ['id' => 'list',        'name' => 'List'],
+            ['id' => 'show',        'name' => 'Show']
         ]);
     }
 }
