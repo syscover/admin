7c0745b4d14e90aff0d062dcea4d69c0a0adeed8
Carlos Rodríguez Palacín <info@digitalh2.com>
2019-02-04 11:18:11 +0100

    add function add all permissions

--- src/Syscover/Admin/GraphQL/Services/PermissionGraphQLService.php
+++ src/Syscover/Admin/GraphQL/Services/PermissionGraphQLService.php
@@ -1,6 +1,8 @@
 <?php namespace Syscover\Admin\GraphQL\Services;
 
+use Syscover\Admin\Models\Action;
 use Syscover\Admin\Models\Permission;
+use Syscover\Admin\Models\Resource;
 use Syscover\Admin\Services\PermissionService;
 use Syscover\Core\GraphQL\Services\CoreGraphQLService;
 
@@ -22,6 +24,28 @@ class PermissionGraphQLService extends CoreGraphQLService
                 ->where('action_id', $args['action_id'])
                 ->delete();
         }
+
+        return true;
+    }
+
+    public function addAllPermissions($root, array $args)
+    {
+        $permissions = [];
+        $resources = Resource::all();
+        $actions = Action::all();
+
+        foreach ($resources as $resource) {
+            foreach ($actions as $action) {
+                $permissions[] = [
+                    'profile_id'    => $args['profile_id'],
+                    'resource_id'   => $resource->id,
+                    'action_id'     => $action->id
+                ];
+            }
+        }
+
+        Permission::where('profile_id', $args['profile_id'])->delete();
+        Permission::insert($permissions);
 
         return true;
     }
